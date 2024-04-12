c2619662d55f888b658a9afdfa5f11c4cf35054b
Tudor Roman <tudor.roman22@gmail.com>
2024-04-12 08:01:45 +0300

    feat: add lefthook and configure pre-commit commands (#3)
    * feat: add lefthook
    
    * feat: add lefthook and configure pre-commit commands

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # anime-sync-manager
+
 Project to help me organize my anime from one folder to another
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+const config = {
+  semi: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+};
+
+export default config;
